bab3f83c9d691cbaba81273c54310a9c5e8a4281
JamesVillarosa <jvvillarosa@up.edu.ph>
2024-02-20 22:04:52 +0800

    Fourth Commit (Finished with Documentation and Revised README)

--- README.md
+++ README.md
@@ -4,4 +4,29 @@ Title: Simulate a password validation program in JavaScript
 Type: Individual Assessment
 Score: (15 points)
 
+Name: James Carl V. Villarosa
 
+Degree Program: Bachelor of Science in Computer Science
+
+Live Website link: https://github.com/UPLB-CMSC100-AY2324-2S/cmsc100-ay2324-u2l-ex03-JamesVillarosa
+
+Steps on how I created my Exer 03:
+1. I first initialized the hardcoded Passwords and Username.
+2. I created the validatePassword function which includes:
+    * Initialization of variables need in validation.
+    * Checking if 2 passwords are match.
+    * Checking every chars in password if there is
+    a string, number, uppercase, and lowercase.
+    * Checking the condition mention in exercise using the updated value of variables.
+3. I implemented condition that checks if password is valid.
+4. I created reversePassword function which includes:
+    * Initializing empty array for the reverse chars to be stored in.
+    * Traversing and Pushing chars in array.
+    * Concatenating every element in array making it a string.
+    * Returning created reversed password.
+5. I created objectPassword function that creates object with username, actual password, and new password keys.
+
+Key Takeaways:
+1. Familiaring JavaScript syntax.
+2. Familiarization in Data Type, Loops, Array, Condition Statements, Variables, and Objects.
+3. Algorithm creation in JavaScript.
--- index.js
+++ index.js
@@ -1,90 +1,90 @@
 const password1 = "Pass1234";
-const password2 = "Pass1234";
+const password2 = "Pass1234";           // Hard Coded passwords and username.
 const username = "John";
 
-function validatePassword (password1, password2)
+function validatePassword (password1, password2)        // Function that validates password.
 {
     var char = 0;
-    var number = 0;
+    var number = 0;                         // Initialization of variables to be use in validation.
     var uppercase = 0;
     var lowercase = 0;
     var isnumber = false;
     
-    if (password1 == password2)
+    if (password1 == password2)                     // If the two passwords are matched.
     {
-        for (i = 0; i<password1.length; i++)
+        for (i = 0; i<password1.length; i++)            // Traversing the characters in password.
         {
-            if (typeof(password1[i])=="string")
+            if (typeof(password1[i])=="string")             // If char is string, increment string count.
             {
                 char++;
             }
-            if (password1[i] >= 0 && password1[i] <= 9) 
-            {
+            if (password1[i] >= 0 && password1[i] <= 9)     // If char is number, increment number and number will be true
+            {                                               // so that it will not be included in upper/lowercase count.
                 number++;
                 isnumber = true;
             }
-            if (password1[i] == password1[i].toUpperCase() && isnumber==false)
+            if (password1[i] == password1[i].toUpperCase() && isnumber==false)      // If char is uppercase, increment uppercase count.
             {
                 uppercase++;
             }
-            if (password1[i] == password1[i].toLowerCase() && isnumber==false)
+            if (password1[i] == password1[i].toLowerCase() && isnumber==false)      // If char is lowercase, increment lowercase count.
             {
                 lowercase++;
             }
         }
 
-        if (char < 8)
+        if (char < 8)                                                               // Password must have at least 8 chars.
         {
             console.log("Insufficient Number of Strings!");
             return false;
         }
-        else if (number < 1 || uppercase < 1 || lowercase < 1)
-        {
+        else if (number < 1 || uppercase < 1 || lowercase < 1)                      // Password must have at least 1 number, 1 uppercase,
+        {                                                                           // and 1 lower case character.
             console.log("Invalid Password!");
             return false;
         }
         else
         {
-            return true;
+            return true;                                                            // Return true if all conditions are satisfied.
         }
 
-    var isnumber = false;
+    var isnumber = false;                                                           // Returning isnumber to false for other chars.
     }
     else
     {
-        console.log("Wrong Password!");
+        console.log("Wrong Password!");                                             // If the two passwords are not match.
         return false;
     }
 }
 
-function reversePassword(password1)
+function reversePassword(password1)                                         // Function that reverse the chars in valid password.
 {
-    let reversedPassword_array = [];
+    let reversedPassword_array = [];                                        // Initializing empty array for the reversed char to store.
 
-    for (i = password1.length-1; i >= 0; i--)
+    for (i = password1.length-1; i >= 0; i--)                               // Traversing password chars in reverse order.
     {
-        reversedPassword_array.push(password1[i]);
+        reversedPassword_array.push(password1[i]);                          // Pushing chars in array in reversed order.
     }
-    let reversedPassword = reversedPassword_array.join("");
-    return reversedPassword;
+    let reversedPassword = reversedPassword_array.join("");                 // Combining elements of array into string.
+    return reversedPassword;                                                // Returning reversed password.
 }
 
-function objectPassword (password1, reversedPassword, username)
-{
+function objectPassword (password1, reversedPassword, username)             // Function that makes object with username, actual password,
+{                                                                           // and new password as its keys.
         let account = 
         {
-            username: username,
-            actual_password: password1,
+            username: username,                                             // Creating the object named account with username,
+            actual_password: password1,                                     // actual password, and new password keys.
             new_password: reversedPassword
         }
 
-        return account;
+        return account;                                                     // Returning created object.
 }
 
-var validated = validatePassword(password1,password2);
+var validated = validatePassword(password1,password2);      // Function call for validatePassword with password 1 and 2 parameter.
 
-if (validated)
+if (validated)                                                      // If password is valid, password will be reverse and object will be created.
 {
-    reversedPassword = reversePassword(password1);   
-    console.log(objectPassword(password1, reversedPassword, username));
+    reversedPassword = reversePassword(password1);                  // Call for reversedPassword function with parameter password1/2
+    console.log(objectPassword(password1, reversedPassword, username));         // Call and Print the returned value of objectPassword function.
 }
